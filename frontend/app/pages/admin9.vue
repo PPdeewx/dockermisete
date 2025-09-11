d5de25664a116283e809e1fa41816a3e01af0de7
mintsunisa <66022972@up.ac.th>
2025-09-11 18:33:56 +0700

    admin9

--- frontend/app/pages/admin9.vue
+++ frontend/app/pages/admin9.vue
@@ -4,26 +4,28 @@
       <div class="sidebar-header">
         <span>MIS ETE</span>
       </div>
-      <ul class="nav-menu">
-        <li class="nav-item">
-          <a href="#" class="nav-link"><i class="fas fa-home"></i> หน้าหลัก</a>
-        </li>
+        <ul class="nav-menu">
+         <li class="nav-item">
+       <a href="/admin" class="nav-link" @click.prevent="goToAdminPage">
+     <i class="fas fa-home"></i> หน้าหลัก
+   </a>
+</li>
         <li class="nav-item has-submenu active">
           <a href="#" class="nav-link"><i class="fas fa-users"></i> บุคลากร</a>
           <ul class="submenu active">
-            <li><a href="#" class="submenu-link">พนักงานปัจจุบัน</a></li>
-            <li><a href="#" class="submenu-link">พนักงานที่ลาออก</a></li>
-            <li><a href="#" class="submenu-link">บุคลากรภายนอก</a></li>
-            <li><a href="#" class="submenu-link">พนักงาน EDDP</a></li>
-            <li><a href="#" class="submenu-link">เพิ่ม/แก้ไข/ลบ พนักงาน</a></li>
-            <li><a href="#" class="submenu-link">เพิ่มบุคลากรภายนอก</a></li>
-            <li><a href="#" class="submenu-link">เปลี่ยนสถานะพนักงาน</a></li>
+            <li><a href="/admin2" class="submenu-link">พนักงานปัจจุบัน</a></li>
+            <li><a href="/admin3" class="submenu-link">พนักงานที่ลาออก</a></li>
+            <li><a href="/admin4" class="submenu-link">บุคลากรภายนอก</a></li>
+            <li><a href="/admin5" class="submenu-link">พนักงาน EDDP</a></li>
+            <li><a href="/admin6" class="submenu-link">เพิ่ม/แก้ไข/ลบ พนักงาน</a></li>
+            <li><a href="/admin7" class="submenu-link">เพิ่มบุคลากรภายนอก</a></li>
+            <li><a href="/admin8" class="submenu-link">เปลี่ยนสถานะพนักงาน</a></li>
             <li><a href="#" class="submenu-link active">กำหนดโควต้าลา(ทั้งหมด)</a></li>
           </ul>
         </li>
-        <li class="nav-item"><a href="#" class="nav-link"><i class="fas fa-flask"></i> ห้องวิจัย</a></li>
-        <li class="nav-item"><a href="#" class="nav-link"><i class="fas fa-calendar-alt"></i> วันหยุด</a></li>
-        <li class="nav-item"><a href="#" class="nav-link"><i class="fas fa-cog"></i> ระบบการปฏิบัติงาน</a></li>
+        <li class="nav-item"><a href="/admin10" class="nav-link" @click.prevent="goToAdmin10Page"><i class="fas fa-flask"></i> ห้องวิจัย</a></li>
+        <li class="nav-item"><a href="/admin11" class="nav-link" @click.prevent="goToAdmin11Page"><i class="fas fa-calendar-alt"></i> วันหยุด</a></li>
+        <li class="nav-item"><a href="/admin12" class="nav-link" @click.prevent="goToAdmin12Page"><i class="fas fa-cog"></i> ระบบการปฏิบัติงาน</a></li>
       </ul>
     </div>
 
@@ -139,6 +141,22 @@ const goTo = (path: string) => {
   router.push(path);
 };
 
+const goToAdminPage = () => {
+  router.push('/admin');
+};
+
+const goToAdmin10Page = () => {
+  window.location.href = '/admin10';
+};
+
+const goToAdmin11Page = () => {
+  window.location.href = '/admin11';
+};
+
+const goToAdmin12Page = () => {
+  window.location.href = '/admin12';
+};
+
 const filter = reactive({
   status: 'ทั้งหมด',
   department: 'ทั้งหมด',
@@ -214,6 +232,9 @@ const saveQuotaChanges = async () => {
   try {
     await axios.post('http://localhost:8000/api/leave/leave-quotas/bulk_update/', payload)
     alert('บันทึกข้อมูลเรียบร้อย')
+  
+    router.push('/admin12');
+
   } catch (err) {
     console.error(err)
     alert('เกิดข้อผิดพลาด')
