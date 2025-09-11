99cb866dc19cec7bb3dc833b05ffca9fa77ccf0b
mintsunisa <66022972@up.ac.th>
2025-09-11 11:59:09 +0700

    admin28

--- frontend/app/pages/admin28.vue
+++ frontend/app/pages/admin28.vue
@@ -5,14 +5,16 @@
         <span>MIS ETE</span>
       </div>
         <ul class="nav-menu">
-          <li class="nav-item">
-        <a href="/admin" class="nav-link" @click.prevent="goToAdminPage">
+         <li class="nav-item">
+       <a href="/admin" class="nav-link" @click.prevent="goToAdminPage">
      <i class="fas fa-home"></i> หน้าหลัก
    </a>
 </li>
         <li class="nav-item has-submenu">
-          <a href="#" class="nav-link"><i class="fas fa-users"></i> บุคลากร</a>
-          <ul class="submenu">
+          <a href="/admin2" class="nav-link"@click.prevent="goToAdmin2Page">
+            <i class="fas fa-users"></i> บุคลากร
+          </a>
+            <ul class="submenu">
             <li><a href="#" class="submenu-link">พนักงานปัจจุบัน</a></li>
             <li><a href="#" class="submenu-link">พนักงานที่ลาออก</a></li>
             <li><a href="#" class="submenu-link">บุคลากรภายนอก</a></li>
@@ -23,9 +25,9 @@
             <li><a href="#" class="submenu-link">กำหนดโควต้าลา(ทั้งหมด)</a></li>
           </ul>
         </li>
-        <li class="nav-item"><a href="#" class="nav-link"><i class="fas fa-flask"></i> ห้องวิจัย</a></li>
-        <li class="nav-item"><a href="#" class="nav-link"><i class="fas fa-calendar-alt"></i> วันหยุด</a></li>
-        <li class="nav-item"><a href="#" class="nav-link"><i class="fas fa-cog"></i> ระบบการปฏิบัติงาน</a></li>
+        <li class="nav-item"><a href="/admin10" class="nav-link" @click.prevent="goToAdmin10Page"><i class="fas fa-flask"></i> ห้องวิจัย</a></li>
+        <li class="nav-item"><a href="/admin11" class="nav-link" @click.prevent="goToAdmin11Page"><i class="fas fa-calendar-alt"></i> วันหยุด</a></li>
+        <li class="nav-item"><a href="/admin12" class="nav-link" @click.prevent="goToAdmin12Page"><i class="fas fa-cog"></i> ระบบการปฏิบัติงาน</a></li>
       </ul>
     </div>
 
@@ -157,6 +159,21 @@ const goToAdminPage = () => {
   router.push('/admin');
 };
 
+const goToAdmin2Page = () => {
+  window.location.href = '/admin2';
+};
+
+const goToAdmin10Page = () => {
+  window.location.href = '/admin10';
+};
+
+const goToAdmin11Page = () => {
+  window.location.href = '/admin11';
+};
+
+const goToAdmin12Page = () => {
+  window.location.href = '/admin12';
+};
 
 const currentUser = ref<any>(null)
 
